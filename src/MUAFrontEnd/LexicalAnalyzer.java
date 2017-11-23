689b9fbe064d897acc3bfe380b9975abf3598af6
celestialphineas <hanghang0713@hotmail.com>
2017-11-23 21:52:43 +0800

    Refactoring.

--- src/MUAFrontEnd/LexicalAnalyzer.java
+++ src/MUAFrontEnd/LexicalAnalyzer.java
@@ -3,7 +3,6 @@ package MUAFrontEnd;
 // Input: character stream
 // Output: a list of lexemes
 
-import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -125,6 +124,40 @@ public class LexicalAnalyzer {
         return new LinkedList<>(stringList);
     }
 
+    // Tokenize
+    static public List<Token> tokenize(List<String> lexemeList) {
+        if(lexemeList == null) return null;
+        List<Token> result = new LinkedList<>();
+        boolean meetColon = false;
+        for(String lexeme : lexemeList) {
+            if(lexeme == ":") {
+                meetColon = true;
+                continue;
+            }
+            if(meetColon) {
+                meetColon = false;
+                result.add(new Token(Token.Type.OPERATION, "thing"));
+                result.add(new Token(Token.Type.WORD, lexeme));
+            } else {
+                char ch = lexeme.charAt(0);
+                if(ch == '\"') {
+                    result.add(new Token(Token.Type.WORD, lexeme.substring(1)));
+                } else if(ch == '.' || (ch == '-' && lexeme.length() > 1) || Character.isDigit(ch)) {
+                    result.add(new Token(Token.Type.NUMBER, lexeme));
+                } else if(isExpressionSymbol(ch)) {
+                    result.add(new Token(Token.Type.EXPROP, lexeme));
+                } else if(ch == '[') {
+                    result.add(new Token(Token.Type.LBRACKET, lexeme));
+                } else if(ch == ']') {
+                    result.add(new Token(Token.Type.RBRACKET, lexeme));
+                } else {
+                    result.add(new Token(Token.Type.OPERATION, lexeme));
+                }
+            }
+        }
+        return result;
+    }
+
     // MUA character property utilities
     static boolean isBlankChar(char ch) {
         return ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t';
@@ -132,5 +165,8 @@ public class LexicalAnalyzer {
     static boolean isBreakableSymbol(char ch) {
         return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == ':'
                 || ch == '(' || ch == ')' || ch == '[' || ch == ']';
+    }
+    static boolean isExpressionSymbol(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '(' || ch == ')';
     }
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,6 +1,5 @@
 import MUAFrontEnd.LexicalAnalyzer;
 import MUAFrontEnd.Token;
-import MUAFrontEnd.Tokenizer;
 
 import java.util.List;
 import java.util.Scanner;
@@ -9,20 +8,20 @@ public class TestMain {
     public static void main(String[] args) {
         LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
         Scanner scanner = new Scanner(System.in);
-        List<String> stringList = null;
+        List<String> lexemeList = null;
 
         while(scanner.hasNext()) {
             String string = scanner.nextLine();
             lexicalAnalyzer.sendLine(string);
             if(lexicalAnalyzer.isCompleteLine()) {
-                stringList = lexicalAnalyzer.getStringList();
+                lexemeList = lexicalAnalyzer.getStringList();
                 lexicalAnalyzer.cleanUp();
                 break;
             }
         }
 
-        List<Token> tokens = Tokenizer.tokenize(stringList);
-        if(stringList != null) for(Token t: tokens) {
+        List<Token> tokens = LexicalAnalyzer.tokenize(lexemeList);
+        if(lexemeList != null) for(Token t: tokens) {
             System.out.print(t);
         }
         System.out.println();
