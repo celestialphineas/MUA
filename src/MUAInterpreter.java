4377a99c8105382c90e16218edf0a7dd687f9d11
celestialphineas <hanghang0713@hotmail.com>
2017-11-29 17:15:25 +0800

    Update.

--- src/MUAInterpreter.java
+++ src/MUAInterpreter.java
@@ -74,14 +74,9 @@ public class MUAInterpreter {
         if(flagInteractiveMode) while (true) {
             try {
                 if (flagShowPrompt)
-                    MUAIO.getInstance().out.print(String.format("%1$-15s", "[In" + inCount + "] := "));
+                    MUAIO.getInstance().out.print(String.format("%1$-10s", "[In" + inCount + "]\t:= "));
                 inCount++;
-                boolean flagFirst = true;
                 while (scanner.hasNext()) {
-                    if (!flagFirst)
-                        if (flagShowPrompt)
-                            MUAIO.getInstance().out.print("> ");
-                    flagFirst = false;
                     String string = scanner.nextLine();
                     lexicalAnalyzer.sendLine(string);
                     if (lexicalAnalyzer.isCompleteLine()) {
@@ -107,7 +102,7 @@ public class MUAInterpreter {
                         if (flagExpressionOut) for (MUAObject obj : result) {
                             if (flagShowPrompt)
                                 MUAIO.getInstance().out
-                                        .print(String.format("%1$-15s", "[Out" + outCount + "] := "));
+                                        .print(String.format("%1$-11s", "[Out" + outCount + "]\t:= "));
                             outCount++;
                             MUAIO.getInstance().out.println(obj);
                         }
